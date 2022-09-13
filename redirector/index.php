c5de1d89ca51993fd0e53cf7e2e69e3d6b80d20a
Joel Lord <joelphy@gmail.com>
2022-09-13 07:11:49 -0400

    Changed redirector service to use php

--- redirector/index.php
+++ redirector/index.php
@@ -1,23 +1,39 @@
-<?php
+<?
 $endpoint = "https://data.mongodb-api.com/app/redirector-odhgb/endpoint/getRoute";
 $url = $endpoint."?route=".$_SERVER['REQUEST_URI'];
 $contents = file_get_contents($url);
 $route = json_decode($contents);
 
-if (is_array($route)) {
+if(!is_array($route) && $route->to) {
+  header('Location: '.$route->to);
+  exit();
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="author" content="Joel Lord">
+  <meta name="image" content="https://www.mongodb.com/docs/assets/meta_generic.png">
+
+  <title>MDB.link redirector</title>
+</head>
+
+<body id="page-top" style="display:none">
+
   <h1>Oops, that URL was not found</h1>
   <p>Did you mean to visit any of these sites?</p>
   <ul id="routeList">
     <?php
-    for ($i = 0; $i < count($route); $i++) {
-      echo "<li><a href='".$route[$i]->route."'>".$route[$i]->title."</a></li>";
-    }
+      for ($i = 0; $i < count($route); $i++) {
+        echo "<li><a href='".$route[$i]->route."'>".$route[$i]->title."</a></li>";
+      }
     ?>
   </ul>
   <p>If you were trying to reach one of the landing pages, you can check out the list at <a href="http://landing.mdb.link">landing.mdb.link</a></p>
-<?php
-} else {
-  header('Location: '.$route->to);
-}
-?>
+</body>
+
+</html>
